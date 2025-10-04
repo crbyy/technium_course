d504e28e967dcc78ad2b74ff68469c358c53bbd9
crbyy <ustyushenko1998@mail.ru>
2025-10-05 00:45:52 +0300

    Модуль 4, Урок 15

---
+++ 4_15_BFS.py
@@ -0,0 +1,145 @@
+from collections import deque
+
+
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def display_stack(self):
+        return f'ITEMS: {self.items}'
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.items.pop()
+        else:
+            raise IndexError("Стек пуст")
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+        else:
+            raise IndexError("Стек пуст")
+
+    def size(self):
+        return len(self.items)
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+
+        self.outbound = []
+        self.inbound = []
+
+    def point_to(self, other):
+        self.outbound.append(other)
+        other.inbound.append(self)
+
+    def __str__(self):
+        return f'Node({self.value})'
+
+    def __repr__(self):
+        return f'Node({self.value})' ####
+
+class Graph:
+    def __init__(self, root):
+        self._root = root
+
+    def dfs(self):
+        stack = Stack()
+        visited_node = []
+
+        stack.push(self._root)
+
+        while not stack.is_empty():
+            node = stack.pop()
+            if node not in visited_node:
+                visited_node.append(node)
+
+                for neighbor in reversed(node.outbound):
+                    stack.push(neighbor)
+
+        return visited_node
+
+    def bfs(self):
+        queue = deque()
+        visited = set()
+        result = []
+
+        queue.append(self._root)
+        visited.add(self._root)
+        result.append(self._root)
+
+        while queue:
+            node = queue.popleft()
+            print(node)
+
+            for neighbor in node.outbound:
+                if neighbor not in visited:
+                    queue.append(neighbor)
+                    visited.add(neighbor)
+                    result.append(neighbor)
+
+        return result
+
+
+
+
+
+# a = Node('a')
+# b = Node('b')
+# c = Node('c')
+# d = Node('d')
+# a.point_to(b)
+# b.point_to(c)
+# c.point_to(d)
+# d.point_to(a)
+# b.point_to(d)
+
+
+# a = Node('a')
+# b = Node('b')
+# c = Node('c')
+# a.point_to(b)
+# b.point_to(c)
+
+
+# a = Node('a')
+# b = Node('b')
+# c = Node('c')
+# d = Node('d')
+# e = Node('e')
+# f = Node('f')
+# a.point_to(b)
+# b.point_to(c)
+# c.point_to(d)
+# d.point_to(a)
+# b.point_to(d)
+# b.point_to(f)
+# c.point_to(e)
+
+a = Node('a')
+b = Node('b')
+c = Node('c')
+d = Node('d')
+e = Node('e')
+f = Node('f')
+a.point_to(b)
+b.point_to(f)
+b.point_to(e)
+a.point_to(c)
+c.point_to(d)
+
+
+
+
+
+g = Graph(a)
+
+print(g.bfs())
+
